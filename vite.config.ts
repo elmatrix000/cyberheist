d4eec79a15144b5ab86d7d6dc54ed99d15b55611
elmatrix000 <internetflytrap@proton.me>
2026-04-09 10:06:57 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          gsap: ['gsap', '@gsap/react'],
+        },
+      },
+    },
+  },
+});
